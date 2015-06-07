3d2532a0b44a95df55fc72a4eeb167093dfdfec4
Jahsie Ault <jahsieault@gmail.com>
2015-06-06 22:08:19 -0700

    Iteration before column collapse

--- js/styles.js
+++ js/styles.js
@@ -16,19 +16,41 @@
 // }
 
 
-var divs = $('.rotates'),//fetch all the divs
-    len = divs.length,//number of divs on page
-    randomDiv,//random number that will be generated
-    speed = 1000;//interval in miliseconds for the new div to appear
+// var divs = $('.rotates'),//fetch all the divs
+//     len = divs.length,//number of divs on page
+//     randomDiv,//random number that will be generated
+//     speed = 1000;//interval in miliseconds for the new div to appear
 
-console.log(divs);
-var interval = setInterval(
-                function() { 
-                        randomDiv = Math.floor(Math.random()*len);
-                        // divs.removeClass('flipped');
-                        divs.eq(randomDiv).addClass('show');                         
-                } , speed);
+// console.log(divs);
+// var interval = setInterval(
+//                 function() { 
+//                         randomDiv = Math.floor(Math.random()*len);
+//                         // divs.removeClass('flipped');
+//                         divs.eq(randomDiv).addClass('show');                         
+//                 } , speed);
 
+
+// var flip = function() {
+// 	if () 
+// }
+
+var $aboutClick = $("#about-button");
+var $aboutContent = "<div><h3>About LEO</h3><p>Ultricies pulvinar ac magna, lectus duis dapibus vel. Amet porta aliquet augue, vel est montes, adipiscing? Parturient dolor, auctor eu ac, proin odio montes augue sed, sed integer phasellus auctor trinon plac</p></div>";
+$aboutClick.click(function () {
+	console.log("clicked about");
+	// $("#tall-banner").append($aboutContent).toggleClass("front-2");
+	$("#main-banner-container").toggleClass("flipped");
+	$("#main-info").replaceWith($aboutContent);
+});
+
+var $newsClick = $("#news-button");
+var $newsContent = "<div><h3>What's new</h3><p>Some things a dolor,teger phasellus auctor trinon plac</p></div>";
+$newsClick.click(function () {
+	console.log("clicked news");
+	// $("#tall-banner").append($aboutContent).toggleClass("front-2");
+	$("#about-content").toggleClass("flipped");
+	$("#tall-info").replaceWith($newsContent);
+});
 
 var w = window.innerWidth;
 console.log(w);
